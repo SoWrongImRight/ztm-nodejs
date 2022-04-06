0b04401e715bc1123b62b0be78171f543f14b824
Russ Carroll <info@rcarroll.dev>
2022-04-06 16:27:25 -0400

    Comlpleted Section 12, integrated MongoDB and Mongoose.  Updated Jest testing for the DB.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,8 +4,6 @@ const path = require('path');
 
 const planets = require('./planets.mongo');
 
-const habitablePlanets = []
-
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === "CONFIRMED"
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11     
@@ -21,24 +19,40 @@ function loadPlanetsData() {
     }))
     .on('data', async (data) => {
       if (isHabitablePlanet(data)) {
-        // insert + update = upsert
-  /*       await planets.create({
-          keplerName: data.kepler_name,
-        }); */
+        savePlanet(data);
       }
     })
     .on('error', () => {
       console.log(err)
       reject(err)
     })
-    .on('end', () => {
-      console.log(`${habitablePlanets.length} habitable planets found!`);
+    .on('end', async () => {
+      const countPlanetsFound = (await getAllPlanets()).length;
+      console.log(`${countPlanetsFound} habitable planets found!`);
       resolve()
     });
   })}
 
   async function getAllPlanets() {
-    return await planets.find({});
+    return await planets.find({},{
+      '_id': 0, '__v': 0,
+    });
+  }
+
+  async function savePlanet(planet) {
+    // insert + update = upsert
+    try {  
+      await planets.updateOne({
+        keplerName: planet.kepler_name,
+      }, {
+        keplerName: planet.kepler_name,
+      }, {
+        upsert: true,
+      });
+    } catch(err) {
+      console.error(`Could not save planet:  ${err}`)
+    }
+  
   }
 
   module.exports = {
--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,15 @@
 const http = require('http');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo')
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.DB_URL;
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB Connection Ready!');
-});
-
-mongoose.connection.on('error',(err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
